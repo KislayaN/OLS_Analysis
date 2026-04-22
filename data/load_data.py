766fb37dd81d1273fe18d9ba0b7e5ff6c7544772
Kislaya N <kislaya345@gmail.com>
2026-04-22 21:11:35 +0530

    refactor: made code more structured

--- data/load_data.py
+++ data/load_data.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-dataset = pd.read_csv("data/Admission_Predict.csv")
-
 class Load_data:
     def __init__(self):
         super().__init__()
-        self.dataset = dataset
+        self.dataset = None
         
     def get_data(self):
+        self.dataset = pd.read_csv("data/Admission_Predict.csv")
+        
         self.X_dataframe = self.dataset.iloc[:, :-1]
-        self.target = self.dataset.iloc[:, -1]
+        self.target = self.dataset.iloc[:, [-1]]
         return self.X_dataframe, self.target
