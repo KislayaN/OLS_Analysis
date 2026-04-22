9a74b4ea190e4500eadd19f0632c9f6cb384f0e2
Kislaya N <kislaya345@gmail.com>
2026-04-22 15:07:21 +0530

    refactor: fixed scaling method

--- preprocessing/scaler.py
+++ preprocessing/scaler.py
@@ -11,9 +11,13 @@ class Scaler:
         if self.perform_scaling == False:
             return self.X_train, self.X_test
         
+        nunique = self.X_train.nunique()
+        
+        scalable_cols = [key for key in nunique.keys() if nunique[key] > 10 and key not in ['Serial No.']]
+        
         scaler = StandardScaler()
         
-        X_train_scaled = scaler.fit_transform(self.X_train)
-        X_test_scaled = scaler.transform(self.X_test)
+        X_train_scaled = scaler.fit_transform(self.X_train[scalable_cols])
+        X_test_scaled = scaler.transform(self.X_test[scalable_cols])
         
         return X_train_scaled, X_test_scaled
