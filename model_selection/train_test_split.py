4c922ec335561219df4be5338747c90351fe479b
Kislaya N <kislaya345@gmail.com>
2026-04-22 21:13:45 +0530

    refactor: added some parameters in splitting

--- model_selection/train_test_split.py
+++ model_selection/train_test_split.py
@@ -7,5 +7,5 @@ class Model_Selection:
         self.y_dataframe = y_dataframe.copy()
         
     def split(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X_dataframe, self.y_dataframe)
+        X_train, X_test, y_train, y_test = train_test_split(self.X_dataframe, self.y_dataframe, train_size=0.8, random_state=43)
         return X_train, X_test, y_train, y_test
