70a8a77b2f53caa8bc50742e6d3135a6452f1ded
Kislaya N <kislaya345@gmail.com>
2026-04-22 21:12:43 +0530

    made code more structured

--- feature_engineering/feature_engineering.py
+++ feature_engineering/feature_engineering.py
@@ -1,11 +1,10 @@
 class Feature_Engineer:
-    def __init__(self, X_dataframe=None, y_dataframe=None):
+    def __init__(self):
         super().__init__()
-        self.X_dataframe = X_dataframe.copy()
-        self.target = y_dataframe.copy()
         
-    def perform(self):
-        index_col = [col for col in self.X_dataframe.columns if len(self.X_dataframe) == self.X_dataframe[col].nunique()]
+    def perform(self, X_dataframe):
+        X_dataframe = X_dataframe.copy()
+        index_col = [col for col in X_dataframe.columns if len(X_dataframe) == X_dataframe[col].nunique()]
           
-        self.X_dataframe = self.X_dataframe.drop(columns=index_col)
+        self.X_dataframe = X_dataframe.drop(columns=index_col)
         return self.X_dataframe
