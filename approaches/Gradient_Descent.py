97f8e51a72600b90cdf9a96aa490a83c138a32af
Kislaya N <kislaya345@gmail.com>
2026-04-24 10:19:10 +0530

    refactor: fixed calculations

--- approaches/Gradient_Descent.py
+++ approaches/Gradient_Descent.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Gradient_Descent_OLS:
-    def __init__(self, iterations=1000, learning_rate=None, tol=1e-6):
+    def __init__(self, iterations=1000, learning_rate=0.01, tol=1e-6):
         super().__init__()
         self.learning_rate = learning_rate
         self.n_iter = iterations
@@ -15,7 +15,10 @@ class Gradient_Descent_OLS:
         
         self.weights = np.zeros(n)
         
+        y = np.asarray(y).reshape(-1)
+        
         for epoch in range(self.n_iter):
+
             prediction = X_b @ self.weights
             error = prediction - y
             
@@ -24,11 +27,16 @@ class Gradient_Descent_OLS:
             
             # J = (((X_b @ self.weights) - y) ** 2) / m ----- objective function
             
-            del_J = (2/m) * (X_b.T @ ((X_b @ self.weights) - y) )
-            new_weights = self.weights - self.learning_rate * del_J
-
-            if np.linalg.norm(new_weights - self.weights) < self.tol:
-                self.weights = new_weights
+            # del_J = (2/m) * (X_b.T @ ((X_b @ self.weights) - y) )
+            del_J = (2/m) * (X_b.T @ error)
+            
+            if np.linalg.norm(del_J) < self.tol:
                 break
             
+            self.weights -= self.learning_rate * del_J
+            
         return self.weights
+    
+    def predict(self, X):
+        X_b = np.column_stack((np.ones(len(X)), X))
+        return X_b @ self.weights
